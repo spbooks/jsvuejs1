df0aaed17425891450d1dde3738102ed4bccd769
Nilson Jacques <neil.jacques@gmail.com>
2018-12-22 14:44:05 +0000

    Initial commit

---
+++ nuxt.config.js
@@ -0,0 +1,92 @@
+const pkg = require('./package')
+
+module.exports = {
+  mode: 'universal',
+
+  serverMiddleware: ['~/api/auth'],
+
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: pkg.name,
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: pkg.description }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {
+        rel: 'stylesheet',
+        type: 'text/css',
+        href:
+          'https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.3.1/semantic.min.css'
+      }
+    ]
+  },
+
+  router: {
+    linkActiveClass: 'active'
+  },
+
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#fff' },
+
+  /*
+  ** Global CSS
+  */
+  css: [],
+
+  /*
+  ** Plugins to load before mounting the App
+  */
+  plugins: [],
+
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+    // Doc: https://github.com/nuxt-community/axios-module#usage
+    '@nuxtjs/axios',
+    '@nuxtjs/auth'
+  ],
+  /*
+  ** Axios module configuration
+  */
+  axios: {
+    // See https://github.com/nuxt-community/axios-module#options
+  },
+
+  auth: {
+    strategies: {
+      local: {
+        endpoints: {
+          login: { propertyName: 'token.accessToken' }
+        }
+      }
+    }
+  },
+
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** You can extend webpack config here
+    */
+    extend(config, ctx) {
+      // Run ESLint on save
+      if (ctx.isDev && ctx.isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    }
+  }
+}
---
+++ store/index.js
@@ -0,0 +1,53 @@
+import Vuex from 'vuex'
+
+const createStore = () => {
+  return new Vuex.Store({
+    state: () => ({
+      employees: [],
+      selectedEmployee: null
+    }),
+
+    getters: {
+      employeeList(state) {
+        return state.employees.map(member => ({
+          id: member.login.uuid,
+          firstName: member.name.first,
+          lastName: member.name.last,
+          email: member.email,
+          phone: member.phone,
+          nationality: member.nat,
+          avatar: member.picture.thumbnail
+        }))
+      }
+    },
+
+    mutations: {
+      setEmployees(state, { employees }) {
+        state.employees = employees
+      },
+      setSelectedEmployee(state, { employee }) {
+        state.selectedEmployee = employee
+      }
+    },
+
+    actions: {
+      async getEmployees({ commit }) {
+        const { results } = await this.$axios.$get(
+          'https://randomuser.me/api/?nat=gb,us,au&results=10&seed=abc'
+        )
+        commit('setEmployees', { employees: results })
+      },
+
+      async getEmployee({ commit, dispatch, state }, { id }) {
+        if (state.employees.length === 0) {
+          await dispatch('getEmployees')
+        }
+
+        const employee = state.employees.find(user => user.login.uuid === id)
+        commit('setSelectedEmployee', { employee })
+      }
+    }
+  })
+}
+
+export default createStore
